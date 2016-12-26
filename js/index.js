b3954a3f4c11400eb422ee50b9ff58e8b8396808
Rakesh Ravishankar <RakeshRavi@ccs.neu.edu>
2016-12-26 00:41:21 -0500

    Initial form of landing page
    Created a navbar
    created a fade effect on image
    added typed.js package
    created placeholders for content

---
+++ js/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+	$('#dp').css('width', $(window).width());
+	$('#dp').css('height', $(window).height());
+
+	$('#hello-text').css('top', $(window).height() / 1.8);
+	// $('#hello-text').css('left', $(window).width() / 2);
+	var startedTyping = false;
+
+	var maxScroll = 200;//$('#dp').height() - $(window).height();
+	$(window).scroll(function(){
+		$('#dp').css('opacity', (maxScroll - $(window).scrollTop()) / maxScroll);
+		var newTextOpacity = $(window).scrollTop() / maxScroll;
+		if (!startedTyping && newTextOpacity > 0.6) {
+			simulateTyping();
+			startedTyping = true;
+		}
+		$('#hello-text').css('opacity', newTextOpacity);
+	});
+	
+});
+
+
+function simulateTyping()
+{
+
+	$("#hello-text").typed({
+		strings: ["Hello I'm Rakesh.."],
+		typeSpeed: 0.5
+	});
+
+}
