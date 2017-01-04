1493b4de74b798880cb8082c1f985faa22f202af
Rakesh Ravishankar <RakeshRavi@ccs.neu.edu>
2017-01-04 00:40:43 -0500

    New Landing Page

--- js/index.js
+++ js/index.js
@@ -1,76 +1,58 @@
 $(document).ready(function(){
+	
+	$("#car").css({"left": $(window).width() * 0.9, "top": $(window).height() / 4});
+	$("#van").css({"left": $(window).width(), "top": $(window).height() / 4});
+	$("#suv").css({"left": $(window).width() * 0.85, "top": $(window).height() / 3.6});
 
-	$('#dp').css('width', $(window).width());
-	$('#dp').css('height', $(window).height());
+	$("#chopper").css({"left": $(window).width() * 0.5});
+	$("#chopper").animate({
+		top: $(window).height() / 8
+	}, 5000, function(){
+		$("#chopper").css({"transform": "rotate(20deg)"});
 
-	$('#hello-text').css('top', $(window).height() / 1.8);
-	var startedTyping = false;
+		$("#van").animate({
+			left: $(window).width() * 0.9
+		}, 5000, function(){
+			$(this).css("opacity", 0);
+			$("#explosion").css({"width": "7em", "height": "7em", "left": $(window).width() * 0.9, "top": $(window).height() / 4});
+			$("#explosion").attr("src", "resources/explosion.gif");
 
-	var maxScroll = 200;//$('#dp').height() - $(window).height();
-	$(window).scroll(function(){
-		$('#dp').css('opacity', (maxScroll - $(window).scrollTop()) / maxScroll);
-		var newTextOpacity = $(window).scrollTop() / maxScroll;
-		if (!startedTyping && newTextOpacity > 0.6) {
-			simulateTyping();
-			startedTyping = true;
-			animateWebIcon();
-			animateGameIcon();
-		}
-		$('#hello-text').css('opacity', newTextOpacity);
+			$("#explosion").fadeOut("slow");
+		});
+
+
+		$("#car").animate({
+			left: $(window).width() * 0.5
+		}, 9000);
+
+		
+		$("#suv").animate({
+			left: $(window).width() * 0.5
+		}, 9000);
 	});
+
+	$("#chopper").animate({
+		left: $(window).width() * 0.9
+	}, 10000, function(){
+		$("#chopper").css({"transform": "rotate(0deg)"});
+	});
+
+
+
+	$("#balloon").css({"left": $(window).width() * 0.9});
+	$("#balloon").animate({
+		top: $(window).height() * 0.7,
+	}, 6000);
+	$("#balloon").animate({
+		left: $(window).width() * 0.7
+	}, 10000, function(){
+		$("#balloon").animate({
+			top: "5%",
+		}, 6000);
+	});
+
+
+
+
 
 });
-
-
-function simulateTyping()
-{
-	$("#hello-text").typed({
-		strings: ["Hello.. I'm Rakesh.."],
-		typeSpeed: 0.4,
-		showCursor: false
-	});
-}
-
-
-function animateWebIcon()
-{
-	$('#web-dev-icon').fadeTo("slow", 1);
-	$('#web-dev-icon').animate({
-		"margin-left": "10%",
-		"margin-right": "10%"
-	}, 1000, function(){
-		$(this).css("display", "inline");
-		$(this).closest('div').attr("class", "col-md-4");
-		$(this).closest('div').after("<div class='col-md-8 well' id='web-dev-content'></div>");
-		$("#web-dev-content").css({"display": "inline", "background": "#323232", "color": "white", "text-align": "left"});
-		$("#web-dev-content").load("../content/web-dev-content.html");
-
-	});
-}
-
-function animateGameIcon()
-{
-	$('#game-dev-icon').fadeTo("slow", 1);
-	$('#game-dev-icon').animate({
-		"margin-left": "10%",
-		"margin-right": "10%"
-	}, 1000, function(){
-		$(this).css("display", "inline");
-		$(this).closest('div').attr("class", "col-md-4");
-		$(this).closest('div').after("<div class='col-md-8 well' id='game-dev-content'></div>");
-		$("#game-dev-content").css({"display": "inline", "background": "#323232", "color": "white", "text-align": "left"});
-		$("#game-dev-content").load("../content/game-dev-content.html");
-	});
-}
-
-
-
-
-
-
-
-
-
-
-
-
